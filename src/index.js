c5039925751f7aa065b95b1172e431de9dac7570
Pavel Zelenko <43108877+paxom4ik4@users.noreply.github.com>
2020-03-06 14:58:26 +0300

    init: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-module.exports = function toReadable (number) {
-  
+const NUMS = {
+    0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
+    6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
+    11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen',
+    15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen',
+    19: 'nineteen', 20: 'twenty', 30: 'thirty', 40: 'forty',
+    50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty',
+    90: 'ninety', 100: 'hundred'
+   
+};
+
+module.exports = function toReadable(number) {
+    let str = number + '';
+    let lengthOfNumber = str.length;
+    let result = [];
+
+    if (lengthOfNumber === 1 || number <= 20) {
+        result.push(NUMS[number]);
+    }
+
+    if (lengthOfNumber === 2 && number > 20) {
+        result.push(NUMS[str[0] + '0']);
+
+        if (number % 10) {
+            result.push(NUMS[str[1]]);
+        }
+    }
+
+    if (lengthOfNumber === 3) {
+        result.push(NUMBS[str[0]]);
+        result.push(NUMBS['100']);
+
+        if (number % 100) {
+            number = number % 100;
+            str = number + '';
+            lengthOfNumber = str.length;
+        }
+    }
+    return result.join(' ');
 }
