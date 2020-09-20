b29172a14e2e40b4d155d8e844f1c84b8cb98e99
Pavel Zelenko <43108877+paxom4ik4@users.noreply.github.com>
2020-09-20 18:07:31 +0300

    feat: update Human-Readable-Number task

--- src/index.js
+++ src/index.js
@@ -1,36 +1,65 @@
-module.exports = function toReadable (number) {
-    
-    let first = ["","one","two","three","four","five","six","seven","eight","nine","ten",
-                "eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen",
-                "eighteen","nineteen"];
-    let second = ["","","twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"];
-  
-    if (number == 0) return "zero";
+module.exports = function toReadable(number) {
+    let first = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    let second = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
 
-    if (number <= 19) return first[number];
+    if (number === 0) "zero";
+
+    if (number <= 19) first[number];
     else {
         let numString = String(number);
         let result;
         switch (numString.length) {
-            case 2:{
-                result = second[numString[0]] +" "+ first[numString[1]];
+            case 2: {
+                result = second[numString[0]] + " " + first[numString[1]];
                 break;
             }
             case 3: {
                 result = first[numString[0]] + " hundred ";
                 if (numString[1] == "1") {
                     result += first[numString.slice(1)];
+                } else {
+                    if (numString[1] == "0") {
+                        result += first[numString[2]];
+                    } else
+                        result +=
+                            second[numString[1]] + " " + first[numString[2]];
                 }
-                else {
-                  if (numString[1] == "0") {
-                    result += first[numString[2]];
-                  } else
-                     result += second[numString[1]] + " " + first[numString[2]]; 
-              }
                 break;
             }
         }
-      return result.trim();
+        return result.trim();
     }
-  }
-  
+};
+
